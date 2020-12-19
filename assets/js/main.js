33c9d7ad530b9e2c92d847f19f2f9f66eb42adb7
Claudia Nunez <65322589+claujnunez@users.noreply.github.com>
2020-12-18 18:54:59 -0600

    develop Countdown with date added

--- assets/js/main.js
+++ assets/js/main.js
@@ -96,13 +96,14 @@
     $(".mobile-nav, .mobile-nav-toggle").hide();
   }
 
-  // // Intro carousel
-  // var heroCarousel = $("#heroCarousel");
+  // Intro carousel
+  var heroCarousel = $("#heroCarousel");
 
-  // heroCarousel.on('slid.bs.carousel', function(e) {
-  //   $(this).find('h2').addClass('animate__animated animate__fadeInDown');
-  //   $(this).find('p, .btn-get-started').addClass('animate__animated animate__fadeInUp');
-  // });
+  heroCarousel.on('slid.bs.carousel', function (e) {
+    $(this).find('h2').addClass('animate__animated animate__fadeInDown');
+    $(this).find('p, .btn-get-started').addClass('animate__animated animate__fadeInUp');
+  });
+
   // Back to top button
   $(window).scroll(function () {
     if ($(this).scrollTop() > 100) {
@@ -185,3 +186,32 @@
   });
 
 })(jQuery);
+
+// Set the date we're counting down to
+var countDownDate = new Date("Mar 01, 2021 15:37:25").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function () {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Output the result in an element with id="demo"
+  document.getElementById("timer").innerHTML = days + "d " + hours + "h "
+    + minutes + "m " + seconds + "s ";
+
+  // If the count down is over, write some text 
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("timer").innerHTML = "Ready to Launch!!!";
+  }
+}, 1000);
